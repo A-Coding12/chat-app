61fdc02aab62faf0431257e008a670a2119bdaa3
Abdellah Chegdali <a.codingorg@gmail.com>
2024-01-08 16:04:38 +0100

    Production version1.1

--- ChatApp/settings.py
+++ ChatApp/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gtqt5uk920*9nriigt%*t)$tvnyn)(wtky*xrahvu3$oo_!kzg'
+SECRET_KEY = os.environ.get("SECRET_KEY", 'secret_key')
+#django-insecure-gtqt5uk920*9nriigt%*t)$tvnyn)(wtky*xrahvu3$oo_!kzg
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG",True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 #CSRF_TRUSTED_ORIGINS = ['','https://*.127.0.0.1']
 # Application definition
